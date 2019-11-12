c97d06bba1d460f77443581a39b061d24f0a0e70
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-12 17:30:45 +0300

    Enable the changes for full functionality

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.settings.SettingsActivity
+import com.example.android.dagger.user.UserManager
 
 import dagger.BindsInstance
 import dagger.Component
@@ -26,6 +28,8 @@ interface AppComponent {
     fun registrationComponent(): RegistrationComponent.Factory
     fun loginComponent(): LoginComponent.Factory
     // Classes that can be injected by this Component
-    fun inject(activity: MainActivity)
+    // 2) Expose UserManager so that MainActivity and SettingsActivity
+    // can access a particular instance of UserComponent
+    fun userManager(): UserManager
 }
 

9372b1cd58ee251dbe8dc3bc5fbab510a1874ed9
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-12 17:01:20 +0300

    Changes to factor in the login activityies but not the settings activity

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,11 +1,10 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
-import com.example.android.dagger.registration.enterdetails.EnterDetailsError
-import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
-import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
+
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -25,6 +24,7 @@ interface AppComponent {
 
     // Expose RegistrationComponent factory from the graph
     fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
     // Classes that can be injected by this Component
     fun inject(activity: MainActivity)
 }
